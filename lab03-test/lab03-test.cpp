bd28f19ea2c2e48b8715cb491ffd18833210e76f
GrayEllipse <139046145+GrayEllipse@users.noreply.github.com>
2023-07-09 15:59:00 +0300

    code: добавлены тесты для функции find_minmax

---
+++ lab03-test/lab03-test.cpp
@@ -0,0 +1,28 @@
+﻿#include <cassert>
+#include "histogram.h"
+
+void test_positive() {
+    double min = 0;
+    double max = 0;
+
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+
+    find_minmax({ 1, 2, 3 }, min, max);
+    assert(min == 1);
+    assert(max == 3);
+
+    find_minmax({ -5, -2, -4 }, min, max);
+    assert(min == -5);
+    assert(max == -2);
+
+    find_minmax({ 1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+
+}
+
+int main() {
+    test_positive();
+}
