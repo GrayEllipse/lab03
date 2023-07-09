7b351611f1e0e7fcdba4c4e63a660e57d9f0b58e
GrayEllipse <139046145+GrayEllipse@users.noreply.github.com>
2023-07-09 15:02:48 +0300

    code: добавлено зеркальное отображение гистограммы

--- lab03.cpp
+++ lab03.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 
 using namespace std;
 
@@ -21,8 +22,15 @@ void svg_text(double left, double baseline, string text) {
     cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>" << '\n';
 }
 
-void svg_rect(double x, double y, double width, double height, string stroke, string fill) {
-    cout << "<rect x=' " << x << "' y=' " << y << "' width=' " << width << "' height=' " << height << "' stroke=' " << stroke << "' fill='" << fill << "'/>" << '\n';
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black") {
+    vector<string> colors = { "#FFD1DC", "#77DD77", "#FCE883", "#9ACEEB", "#D8BFD8",
+                              "#F0E68C", "#7FC7FF", "#AFEEEE", "#CCCCFF", "#3EB489" };
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(0, 9);
+    string color = colors[distr(gen)];
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << color << "' fill='" << color << "'/>" << '\n';
 }
 
 size_t find_max_count(const vector<size_t>& bins, const size_t bin_count) {
@@ -38,9 +46,8 @@ size_t find_max_count(const vector<size_t>& bins, const size_t bin_count) {
 void show_histogram_svg(const vector<size_t>& bins, const size_t& bin_count) {
     const auto IMAGE_WIDTH = 810;
     const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
     const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
+    const auto TEXT_WIDTH = 10;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
     const size_t MAX_NUMBER_OF_CHAR = 80;
@@ -54,11 +61,15 @@ void show_histogram_svg(const vector<size_t>& bins, const size_t& bin_count) {
     if (max_count > MAX_ASTERISK)
         scaling_factor = MAX_ASTERISK / static_cast<double>(max_count);
     size_t scaled_bin;
-    for (size_t bin : bins) {
+    size_t max_height = static_cast<size_t>(scaling_factor * max_count);
+    const auto TEXT_LEFT = max_height * BLOCK_WIDTH + TEXT_WIDTH;
+    double indent;
+    for (size_t bin : bins) { 
         scaled_bin = static_cast<size_t>(scaling_factor * bin);
+        indent = BLOCK_WIDTH * (max_height - scaled_bin);
         const double bin_width = BLOCK_WIDTH * scaled_bin;
+        svg_rect(indent, top, bin_width, BIN_HEIGHT);
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
         top += BIN_HEIGHT;
     }
     svg_end();
@@ -90,15 +101,25 @@ vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_co
     double min, max;
     find_minmax(numbers, min, max);
     double bin_size = (max - min) / bin_count;
-    for (double number : numbers) {
-        size_t bin = static_cast<size_t>((number - min) / bin_size);
-        if (bin == bin_count) {
-            bin--;
+    size_t max_count = 0;
+    for (size_t i = 0; i < numbers.size(); i++) {
+        bool found = false;
+        size_t j;
+        for (j = 0; (j < bin_count - 1) && !found; j++) {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+            if ((lo <= numbers[i]) && (numbers[i] < hi)) {
+                bins[j]++;
+                found = true;
+            }
         }
-        bins[bin]++;
+        if (!found) {
+            bins[bin_count - 1]++;
+        }
     }
     return bins;
 }
+
 
 void show_histogram_text(const vector<size_t>& bins, const size_t bin_count) {
     const size_t SCREEN_WIDTH = 80;
