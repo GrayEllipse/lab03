2b0a21817e369cdb22f5022253c9f21562969f82
GrayEllipse <139046145+GrayEllipse@users.noreply.github.com>
2023-07-09 15:15:49 +0300

    code: добавлен модуль histogram

--- lab03.cpp
+++ lab03.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <random>
+#include "histogram.h"
 
 using namespace std;
 
@@ -82,20 +83,6 @@ vector<double> input_numbers(size_t count) {
     }
     return result;
 }
-
-void find_minmax(const vector<double>& numbers, double& min, double& max) {
-    min = numbers[0];
-    max = numbers[0];
-    for (double x : numbers) {
-        if (x < min) {
-            min = x;
-        }
-        else if (x > max) {
-            max = x;
-        }
-    }
-}
-
 vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_count) {
     vector<size_t> bins(bin_count);
     double min, max;
