6a412f1b97d4acc1203f14ba08e9c84605373a28
GrayEllipse <139046145+GrayEllipse@users.noreply.github.com>
2023-07-09 13:44:58 +0300

    code: декомпозиция программы функциями

--- lab03.cpp
+++ lab03.cpp
@@ -2,24 +2,21 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <string>
 
 using namespace std;
 
-int main()
-{
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-    vector<double> numbers(number_count);
-    for (int i = 0; i < number_count; i++)
-        cin >> numbers[i];
-    size_t bin_count;
-    cerr << "Enter bin count: ";
-    cin >> bin_count;
-    vector<size_t> bins(bin_count);
-    double min = numbers[0];
-    double max = numbers[0];
+
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
+}
+
+void find_minmax(const vector<double>& numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
     for (double x : numbers) {
         if (x < min) {
             min = x;
@@ -28,9 +25,15 @@ int main()
             max = x;
         }
     }
+}
+
+vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_count) {
+    vector<size_t> bins(bin_count);
+    double min, max;
+    find_minmax(numbers, min, max);
     double bin_size = (max - min) / bin_count;
     size_t max_count = 0;
-    for (size_t i = 0; i < number_count; i++) {
+    for (size_t i = 0; i < numbers.size(); i++) {
         bool found = false;
         size_t j;
         for (j = 0; (j < bin_count - 1) && !found; j++) {
@@ -39,18 +42,29 @@ int main()
             if ((lo <= numbers[i]) && (numbers[i] < hi)) {
                 bins[j]++;
                 found = true;
-                if (bins[j] > max_count)
-                    max_count = bins[j];
             }
         }
         if (!found) {
             bins[bin_count - 1]++;
-            if (bins[bin_count - 1] > max_count)
-                max_count = bins[bin_count - 1];
         }
     }
+    return bins;
+}
+
+size_t find_max_count(const vector<size_t>& bins, const size_t bin_count) {
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    return max_count;
+}
+
+void show_histogram_text(const vector<size_t>& bins, const size_t bin_count) {
     double scaling_factor = 1.0;
     size_t height;
+    size_t max_count = find_max_count(bins, bin_count);
     if (max_count > 76)
         scaling_factor = 76 / static_cast<double>(max_count);
     size_t max_height = static_cast<size_t>(scaling_factor * max_count);
@@ -68,4 +82,17 @@ int main()
         cout << bins[i];
         cout << '\n';
     }
+}
+
+int main()
+{
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+    auto numbers = input_numbers(number_count);
+    size_t bin_count;
+    cerr << "Enter bin count: ";
+    cin >> bin_count;
+    vector<size_t> bins = make_histogram(numbers, bin_count);
+    show_histogram_text(bins, bin_count);
 }
